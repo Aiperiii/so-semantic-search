3d97d9bebd22716c55392151712e323e3223de9e
Aiperi <aiperiakjoltoeva@gmail.com>
2026-07-12 14:19:49 +0600

    Add expand parameter to search endpoint

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from search import search
 app = FastAPI()
 
 @app.get("/search")
-def search_query(q : str, limit : int  = Query(default=10, ge=1, le=100)):
+def search_query(q : str, limit : int  = Query(default=10, ge=1, le=100), expand: bool = True):
 
-    results = search(q, limit)
+    results = search(q, limit, expand)
     return [{"question_id" : question_id, "title" : title, "score" : score} 
             for question_id, title, score in results]
 
